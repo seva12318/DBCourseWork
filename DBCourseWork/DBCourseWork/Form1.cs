4e8a98801c12159cbbbc1c4ac829f14fcd4cfec8
Vsevolod Sudomoykin <70789213+seva12318@users.noreply.github.com>
2021-11-22 15:00:50 +0800

    Add files via upload

--- DBCourseWork/DBCourseWork/Form1.cs
+++ DBCourseWork/DBCourseWork/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DBCourseWork
 {
     public partial class Form1 : Form
     {
+        private SqlConnection sqlConnection = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ConnectionString);
+            sqlConnection.Open();
+            /*if (sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Right");
+            }*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
